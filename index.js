c600f4b7c2efe446bfe88437642621a10770150c
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2026-08-06 16:21:15 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+//variáveis de ambiente
+require('dotenv').config();
+
+//Conexao com o banco
+require('./libs/dbConnect');
+
+// configurações iniciais
+app.use(morgan('dev'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+const porta = Number(process.env.PORTA) //adicionar
+
+
+
+app.listen(porta, () => {
+    console.log('Servidor rodando');
+    console.log('Endereco: http://localhost:'+porta);
+    // console.log(process.env.variavel); //excluir esta linha
+});
