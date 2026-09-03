a04f923d940efe5969502cbd7346ed023f898a72
TiagoFG-DEV <tiagop05gregorio@gmail.com>
2026-09-03 08:51:44 -0300

    atualizando o projeto

--- backup/codigo/routes/cadastro.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cadastroController = require('../controllers/cadastro.controller');
-
-router.get('/cadastro', cadastroController.renderCadastro);
-router.post('/cadastro', cadastroController.postCadastro);
-
-module.exports = router;
--- backup/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-router.get('/login', authController.renderLogin);
-router.post('/login', authController.postLogin);
-router.get('/logout', authController.logout);
-
-module.exports = router;
